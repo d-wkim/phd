2db94e0733ba432a34d85cadd59afe8c4419743d
dong-wkim <admin@dongwkim.org>
2026-07-21 11:31:11 +0200

    Automatic update

--- src/.ipynb_checkpoints/badges-checkpoint.py
+++ src/.ipynb_checkpoints/badges-checkpoint.py
@@ -1,22 +1,6 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.19.4
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# %%
-from IPython.display import display, HTML
 
 def static_badge(link, label, color, logo = "", logo_color = "", logo_size = 30):
+    from IPython.display import display, HTML
     if logo == "":
         badge = f"""<div align="center">
     <a href="{link}"><img alt="{label}" src="https://img.shields.io/badge/{label.replace(" ", "_")}-{color}?style=for-the-badge"  width="{logo_size}"></a>
